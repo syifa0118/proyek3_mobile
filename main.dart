1b9f216db463c9dc79bebf2185010e5d49892250
syifa0118 <syifasyauqiyah0118@gmail.com>
2026-04-30 11:02:04 +0700

    update

--- main.dart
+++ main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'pages/form_penitipan_page.dart';
-import 'pages/form_grooming_page.dart';
+import 'pages/home_page.dart';
+import 'pages/grooming_page.dart';
+import 'pages/penitipan_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,95 +12,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MenuPage(),
-    );
-  }
-}
 
-class MenuPage extends StatelessWidget {
-  const MenuPage({super.key});
+      initialRoute: '/',
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xff48618F),
-
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 40),
-
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-
-              /// TOMBOL GROOMING
-              SizedBox(
-                width: double.infinity,
-                height: 55,
-
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    foregroundColor: const Color(0xff162D5C),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                  ),
-
-                  child: const Text(
-                    "Form Grooming",
-                    style: TextStyle(fontSize: 18),
-                  ),
-
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const FormGroomingPage(),
-                      ),
-                    );
-                  },
-                ),
-              ),
-
-              const SizedBox(height: 20),
-
-              /// TOMBOL PENITIPAN
-              SizedBox(
-                width: double.infinity,
-                height: 55,
-
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    foregroundColor: const Color(0xff162D5C),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                  ),
-
-                  child: const Text(
-                    "Form Penitipan",
-                    style: TextStyle(fontSize: 18),
-                  ),
-
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const FormPenitipanPage(),
-                      ),
-                    );
-                  },
-                ),
-              ),
-
-            ],
-          ),
-        ),
-      ),
+      routes: {
+        '/': (context) => const HomePage(),
+        '/grooming': (context) => GroomingPage(),
+        '/penitipan': (context) => PenitipanPage(),
+      },
     );
   }
 }
